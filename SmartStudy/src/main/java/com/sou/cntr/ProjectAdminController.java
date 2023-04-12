fd7b59d7d1d7c0b1e00daa550b5b8363ceb0959f
Rohit-Irchalwar <rohitirchalwar34@gmail.com>
2023-04-12 15:08:53 +0530

    Initial commit

---
+++ SmartStudy/src/main/java/com/sou/cntr/ProjectAdminController.java
@@ -0,0 +1,67 @@
+package com.sou.cntr;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sou.model.Internships;
+import com.sou.model.ProjectAdmin;
+import com.sou.model.Student;
+import com.sou.model.Teacher;
+import com.sou.service.InternshipsService;
+import com.sou.service.ProjectAdminService;
+import com.sou.service.StudentService;
+import com.sou.service.TeacherService;
+
+
+@RestController
+@RequestMapping("/projectadmin")
+public class ProjectAdminController {
+
+	@Autowired
+	private ProjectAdminService pserv;
+	@Autowired
+	private InternshipsService iserv;
+	@Autowired
+	private StudentService sserv;
+	@Autowired
+	private TeacherService tserv;
+	
+	@PostMapping(value = {"/projectadminadd"})
+	public ProjectAdmin projectadminAddForm(@RequestBody ProjectAdmin p) {
+		pserv.add(p);
+		return p;
+	}
+	@GetMapping(value = {"/projectadmingetAll"})
+	public List<ProjectAdmin> ProjectAdminList(){
+		return pserv.getAll();
+	}
+	//return axios.post(`${INTERNSHIP_URL}/allstudent`, create); 
+	@GetMapping(value = {"/allstudent"})
+	public List<Student> allStudentList(){
+		return sserv.getAll();
+	}
+	@GetMapping(value = {"/allteacher"})
+	public List<Teacher> allTeacherList(){
+		return tserv.getAll();
+	}
+	@GetMapping(value= {"/getProjectAdminbyID"})
+	public ProjectAdmin getprojectAdminbyID(@RequestParam(name = "id") int id){
+		return pserv.getById(id);
+	}
+	@GetMapping(value= {"/deleteProjectAdmin"})
+	public void deleteprojectadminbyID(@RequestParam(name = "id") int id){
+		pserv.removeById(id);
+	}
+	@PostMapping(value = {"/addinternship"})
+	public Internships internshipAddForm(@RequestBody Internships p) {
+		iserv.add(p);
+		return p;
+	}
+}
